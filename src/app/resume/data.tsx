f676b87f99b4a9d184d1338e6390486b96b5163f
Huy <dangquanghuy0204@gmail.com>
2024-07-16 19:20:12 +0700

    add base for resume page

---
+++ src/app/resume/data.tsx
@@ -0,0 +1,132 @@
+import { JSX } from "react";
+import {
+  FaHtml5,
+  FaSass,
+  FaReact,
+  FaNodeJs,
+  FaJava,
+  FaTs,
+} from "react-icons/fa";
+
+import {
+  SiTailwindcss,
+  SiBootstrap,
+  SiNextdotjs,
+  SiExpress,
+  SiNestjs,
+} from "react-icons/si";
+
+type BaseInformationFields<T> = {
+  icon?: string;
+  title: string;
+  items: T[];
+};
+
+type AboutItems = {
+  fieldName: string;
+  fieldValue: string;
+};
+const ABOUT: BaseInformationFields<AboutItems> = {
+  title: "About me",
+  items: [
+    {
+      fieldName: "Name",
+      fieldValue: "Huy Dang",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+84) 905 347 725",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "5+",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Vietnam",
+    },
+    {
+      fieldName: "Based in",
+      fieldValue: "Ho Chi Minh City",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "dangquanghuy0204@gmail.com",
+    },
+  ],
+};
+
+type ExperienceItems = {
+  position: string;
+  company: string;
+  duration: string;
+  description: string;
+};
+const EXPERIENCE: BaseInformationFields<ExperienceItems> = {
+  icon: "",
+  title: "My Experience",
+  items: [
+    {
+      position: "Full Stack Developer",
+      company: "Freelancer",
+      duration: "2021 - Present",
+      description: "Developed web applications using React, Node.js, and Java.",
+    },
+    {
+      position: "Software Engineer",
+      company: "FPT Software",
+      duration: "2019 - 2021",
+      description: "Developed web applications using React, Node.js, and Java.",
+    },
+  ],
+};
+
+type EducationItems = {
+  degree: string;
+  institution: string;
+  duration: string;
+};
+const EDUCATION: BaseInformationFields<EducationItems> = {
+  icon: "",
+  title: "My Education",
+  items: [
+    {
+      degree: "Bachelor of Computer Science",
+      institution: "University of Information Technology",
+      duration: "2015 - 2019",
+    },
+    {
+      degree: "Bachelor of Computer Science",
+      institution: "University of Information Technology",
+      duration: "2015 - 2019",
+    },
+  ],
+};
+
+type SkillItems = {
+  icon: JSX.Element;
+  name: string;
+};
+const SKILLS: BaseInformationFields<SkillItems> = {
+  title: "My skills",
+  items: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML/HTML5",
+    },
+    {
+      icon: <FaSass />,
+      name: "CSS/SASS",
+    },
+    {
+      icon: <FaReact />,
+      name: "ReactJS",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next.js",
+    },
+  ],
+};
+
+export { ABOUT, EDUCATION, EXPERIENCE, SKILLS };
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,53 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { motion } from "framer-motion";
+
 function Resume() {
-  return <div>resume page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeInOut",
+        },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              experience
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              Skills
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              Education
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              About me
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 }
 
 export default Resume;
