0ebec4b15b4a58d600977dbc1e1a55c6dc07fbd5
Huy <dangquanghuy0204@gmail.com>
2024-07-19 23:37:56 +0700

    improve slider buttons

--- src/components/slider-buttons.tsx
+++ src/components/slider-buttons.tsx
@@ -2,22 +2,35 @@
 
 import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+import { cn } from "@/lib/utils";
 
-type SliderButtonProps = Partial<{
-  containerStyles: string;
-  buttonStyles: string;
-  iconStyles: string;
-}>;
+type SliderButtonProps = Partial<
+  {
+    containerStyles: string;
+    buttonStyles: string;
+    iconStyles: string;
+  } & { lastIndex: number }
+>;
 
 function SliderButtons(props: SliderButtonProps) {
   const swiper = useSwiper();
+  const currentIndex = swiper.activeIndex;
+
+  const first = currentIndex == 0;
+  const last = currentIndex == props.lastIndex;
 
   return (
     <div className={props.containerStyles}>
-      <button className={props.buttonStyles} onClick={() => swiper.slidePrev()}>
+      <button
+        className={cn(props.buttonStyles, first && "hidden")}
+        onClick={() => swiper.slidePrev()}
+      >
         <PiCaretLeftBold className={props.iconStyles} />
       </button>
-      <button className={props.buttonStyles} onClick={() => swiper.slideNext()}>
+      <button
+        className={cn(props.buttonStyles, first && "ms-auto", last && "hidden")}
+        onClick={() => swiper.slideNext()}
+      >
         <PiCaretRightBold className={props.iconStyles} />
       </button>
     </div>
